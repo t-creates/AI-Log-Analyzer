06507a3825ad1980091d69ca44c6e08f2b772c35
t-creates <geislinger@pm.me>
2025-12-14 22:58:31 -0700

    env setting for hosted version

--- backend/app/main.py
+++ backend/app/main.py
@@ -31,7 +31,7 @@ os.environ["MKL_NUM_THREADS"] = "1"
 os.environ["OPENBLAS_NUM_THREADS"] = "1"
 os.environ["VECLIB_MAXIMUM_THREADS"] = "1"
 os.environ["NUMEXPR_NUM_THREADS"] = "1"
-
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
 # -------------------------
 # Response helpers (MVP)
